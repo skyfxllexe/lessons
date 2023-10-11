c5bc80bfd3ea87a790f80a621dbc3d1db99ec644
Yan Zerkalin <discxnnectedexehse@gmail.com>
2023-10-12 02:42:57 +0300

    done

--- OneDrive/Desktop/train1.0/train2.0/hw_1/taskD.py
+++
@@ -1,4 +0,0 @@
-N = int(input())
-array = list(map(int,input().split()))
-
-print((array[0]+array[-1])//2)
--- OneDrive/Desktop/train1.0/train2.0/hw_2/taskC.py
+++
@@ -1,9 +0,0 @@
-s = input()
-
-
-minim = 10e10
-for i in range(1,len(s)):
-    print(s+s[0:i])
-    if s + s[0:i][::-1] == (s+s[0:i][::-1])[::-1]:
-        minim = min(minim, len(s+s[0:i]))
-print(minim)
